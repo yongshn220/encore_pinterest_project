55e467d5d99dc60b5e798d3bb1dce9d701836990
Yongjung Shin <yongshn220@gmail.com>
2021-12-29 18:27:14 +0900

    add spring boot project

---
+++ src/main/java/com/encore/project2/EnocreProject2Application.java
@@ -0,0 +1,13 @@
+package com.encore.project2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnocreProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnocreProject2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/encore/project2/EnocreProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.encore.project2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnocreProject2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
