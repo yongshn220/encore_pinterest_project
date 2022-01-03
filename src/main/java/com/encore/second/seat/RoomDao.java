6731f18cd1243f8aa0ed32fb71e8d08d0323c370
Rndns <bible1218@naver.com>
2022-01-03 17:06:31 +0900

    edit2

--- src/main/java/com/encore/second/seat/RoomDao.java
+++ src/main/java/com/encore/second/seat/RoomDao.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoomDao extends JpaRepository<Room, Integer> {
 
-	ArrayList<Room> findByRoom(String room_id);
+	ArrayList<Room> findByRoom(String detail_id);
 }
