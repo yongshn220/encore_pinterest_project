5addf21e93541282c491a9385a9cdf01cc53520a
Yongjung Shin <yongshn220@gmail.com>
2022-01-04 10:31:03 +0900

    add SeatController class

---
+++ src/main/java/com/encore/second/seat/SeatController.java
@@ -0,0 +1,14 @@
+package com.encore.second.seat;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SeatController {
+
+	@Autowired
+	SeatService service;
+	
+	
+	
+}
