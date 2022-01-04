06d90f62bcda119a362c04a2f4db842d08450d24
Rndns <bible1218@naver.com>
2022-01-04 11:11:09 +0900

    asd

--- src/main/java/com/encore/second/movie_detail/Movie_detailController.java
+++ src/main/java/com/encore/second/movie_detail/Movie_detailController.java
@@ -1,5 +1,26 @@
 package com.encore.second.movie_detail;
 
-public class Movie_detailController {
+import java.util.ArrayList;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.encore.second.movie.Movie;
+
+@Controller
+@RequestMapping("/Detail")
+public class Movie_detailController {
+	
+	@Autowired
+	private Movie_detailService service;
+	
+	@GetMapping("/main/{movie}")
+	public void main(Map map) {
+		ArrayList<Movie_detail> list = service.getByMovie_detail_movie(@PathVariable("movie") Movie movie);
+				
+	}
 }
